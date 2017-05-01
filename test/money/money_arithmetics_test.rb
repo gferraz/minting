86b271d77f2c31651237be222984076e97ceeb9f
Gilson Ferraz César <gilson@cesar.etc.br>
2017-04-30 22:54:18 -0300

    Refator arithmetics tests

--- test/money/money_arithmetics_test.rb
+++ test/money/money_arithmetics_test.rb
@@ -1,30 +1,28 @@
 class MoneyArithmeticsTest < Minitest::Test
   USD = Mint::Currency[:USD]
 
+  def setup
+    @two = Mint::Money.new(2r, USD)
+    @six = Mint::Money.new(6r, USD)
+    @ten = Mint::Money.new(10r, USD)
+    @negative_ten = Mint::Money.new(-10r, USD)
+  end
+
   def test_addition
-    two = Mint::Money.new(2r, USD)
-    six = Mint::Money.new(6r, USD)
-    ten = Mint::Money.new(10r, USD)
+    assert_equal @ten, @two + @two + @six
+    assert_equal @ten, @ten + 0
+    assert_equal @ten, 0 + @ten
 
-    assert_equal ten, two + two + six
-    assert_equal ten, ten + 0
-    assert_equal ten, 0 + ten
-
-    assert_raises(TypeError) { ten + 0.0023 }
-    assert_raises(TypeError) { 1.23 + ten }
+    assert_raises(TypeError) { @ten + 0.0023 }
+    assert_raises(TypeError) { 1.23 + @ten }
   end
 
   def test_subtraction
-    two = Mint::Money.new(2r, USD)
-    six = Mint::Money.new(6r, USD)
-    ten = Mint::Money.new(10r, USD)
-    negative_ten = Mint::Money.new(-10r, USD)
+    assert_equal @six, @ten - @two - @two
+    assert_equal @ten, @ten - 0
+    assert_equal(@negative_ten, 0 - @ten)
 
-    assert_equal six, ten - two - two
-    assert_equal ten, ten - 0
-    assert_equal(negative_ten, 0 - ten)
-
-    assert_raises(TypeError) { ten - 0.0023 }
-    assert_raises(TypeError) { 1.23 - ten }
+    assert_raises(TypeError) { @ten - 0.0023 }
+    assert_raises(TypeError) { 1.23 - @ten }
   end
 end
