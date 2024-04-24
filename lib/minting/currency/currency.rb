d3563537954513f620fc002ca31712b08192f36f
Gilson Ferraz <gilson@cesar.etc.br>
2024-04-24 00:02:18 -0300

    Add frozen literal string

--- lib/minting/currency/currency.rb
+++ lib/minting/currency/currency.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Mint
   class Currency
     attr_reader :code, :subunit, :symbol
--- lib/minting/money/money.rb
+++ lib/minting/money/money.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Mint
   class Money
     attr_reader :amount, :currency
