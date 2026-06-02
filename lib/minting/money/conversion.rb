6e77b8671e3eb58a06a86fd9a1c95b565a1d0182
Gilson Ferraz <gilson@cesar.etc.br>
2026-06-02 15:41:52 -0300

    Small code improvements

--- lib/minting/money/conversion.rb
+++ lib/minting/money/conversion.rb
@@ -6,20 +6,13 @@ module Mint
     # Converts the monetary amount to a {BigDecimal} object.
     #
     # @return [BigDecimal] the decimal representation of the money amount
-    # @raise [NoMethodError] if the bigdecimal gem is not loaded/available
-    def to_d
-      raise NoMethodError, 'decimal gem required' unless defined?(BigDecimal)
-
-      amount.to_d 0
-    end
+    def to_d = amount.to_d 0
 
     # Converts the monetary amount to a standard float.
     # Note: Using float conversion loses precision guarantees.
     #
     # @return [Float] the floating-point representation of the money amount
-    def to_f
-      amount.to_f
-    end
+    def to_f = amount.to_f
 
     # Renders a safe HTML5 `<data>` element containing the formatted currency.
     # Embeds the ISO currency description and raw value as the metadata `title` attribute.
@@ -29,15 +22,13 @@ module Mint
     def to_html(format = DEFAULT_FORMAT)
       title = Kernel.format("#{currency_code} %0.#{currency.subunit}f", amount)
       body = to_s(format: format)
-      %(<data class='money' title='#{ERB::Util.html_escape(title)}'>#{ERB::Util.html_escape(body)}</data>)
+      %(<data class='money' title='#{title}'>#{ERB::Util.html_escape(body)}</data>)
     end
 
     # Truncates and converts the monetary amount to an Integer.
     #
     # @return [Integer] the integer representation of the money amount
-    def to_i
-      amount.to_i
-    end
+    def to_i = amount.to_i
 
     def to_hash
       { currency: currency_code, amount: Kernel.format("%0.#{currency.subunit}f", amount) }
@@ -56,8 +47,6 @@ module Mint
     # Returns the exact internal Rational representation of the monetary amount.
     #
     # @return [Rational] the rational representation of the money amount
-    def to_r
-      amount
-    end
+    def to_r = amount
   end
 end
