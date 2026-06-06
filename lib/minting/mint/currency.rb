ac4a909077a4415360d3dfa89590072527a93bf3
Gilson Ferraz <gilson@cesar.etc.br>
2026-06-06 10:19:10 -0300

    Refactor Currency to be a Data structure.

--- lib/minting/mint/currency.rb
+++ lib/minting/mint/currency.rb
@@ -4,31 +4,26 @@ module Mint
   # Represents a specific currency unit, identified by ISO 4217 alphabetic code
   #
   # @see https://www.iso.org/iso-4217-currency-codes.html
-  class Currency
-    attr_reader :code, :subunit, :symbol,
-                :country,
-                :fractional_multiplier, :minimum_amount,
-                :name, :priority
+  Currency = Data.define(:code, :subunit, :symbol, :priority, :country, :name,
+                         :fractional_multiplier, :minimum_amount) do
+    def initialize(code:, symbol:, subunit: 0, priority: 0, country: nil, name: nil)
+      subunit = subunit.to_i
+      priority = priority.to_i
+      fractional_multiplier = 10**subunit
+      minimum_amount = Rational(1, fractional_multiplier)
+      super(code:, subunit:, symbol:, priority:, country:, name:,
+            fractional_multiplier:, minimum_amount:)
+    end
 
-    def inspect = "<Currency:(#{code} #{symbol} #{subunit} #{name})>"
+    def ==(other) = code == other.code
+
+    def inspect
+      "<Currency:(#{code} #{symbol} #{subunit} #{name})>"
+    end
 
     # Normalizes numeric amounts for this currency
     # 1. Converts to Rational
     # 2. Rounds to respect currency subunit
     def normalize_amount(amount) = amount.to_r.round(subunit)
-
-    private
-
-    def initialize(code:, symbol:, subunit: 0, priority: 0, country: nil, name: nil)
-      @code = code
-      @subunit = subunit.to_i
-      @symbol = symbol
-      @priority = priority.to_i
-      @country = country
-      @name = name
-      @fractional_multiplier = 10**@subunit
-      @minimum_amount = Rational(1, fractional_multiplier)
-      freeze
-    end
   end
 end
