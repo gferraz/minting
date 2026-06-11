33e529006914a45a8bd43b18bd75db895f33435b
Gilson Ferraz <gilson@cesar.etc.br>
2026-06-11 01:16:36 -0300

    Fix rdoc adn small code cleaning

--- lib/minting/money/coercion.rb
+++ lib/minting/money/coercion.rb
@@ -4,13 +4,13 @@ module Mint
   # Implements the standard Ruby coercion protocol.
   class Money
     # Allows {Money} to interact seamlessly as the right-hand operand in Numeric arithmetic.
-    # This enables expressions like `5 + money` where `5` is a Numeric and `money` is a Money object.
+    # This enables expressions like `5 * money` where `5` is a Numeric and `money` is a Money object.
     #
     # @param other [Numeric] the left-hand operand to coerce
     # @return [Array(CoercedNumber, Money)] coerced operand array
     # @example
     #   price = Mint.money(10, 'USD')
-    #   5 + price  #=> [USD 15.00] (via coercion)
+    #   5 * price  #=> [USD 50.00] (via coercion)
     def coerce(other)
       [CoercedNumber.new(other), self]
     end
@@ -20,22 +20,20 @@ module Mint
     # @private
     class CoercedNumber
       # @private
-      def initialize(value)
-        @value = value
-      end
+      def initialize(value) = @value = value
 
       # @private
       def +(other)
-        return other if @value.zero?
+        raise_coercion_error(:+, other) unless @value.zero?
 
-        raise_coercion_error(:+, other)
+        other
       end
 
       # @private
       def -(other)
-        return -other if @value.zero?
+        raise_coercion_error(:-, other) unless @value.zero?
 
-        raise_coercion_error(:-, other)
+        -other
       end
 
       # @private
@@ -59,8 +57,7 @@ module Mint
       private
 
       def raise_coercion_error(operation, operand)
-        raise TypeError,
-              "#{@value} #{operation} #{operand} : incompatible operands"
+        raise TypeError, "#{@value} #{operation} #{operand} : incompatible operands"
       end
     end
     private_constant :CoercedNumber
