f6c237d8b71d94bdfaaa1d5ea2f99bef412388a4
Gilson Ferraz <gilson@cesar.etc.br>
2026-06-11 00:15:35 -0300

    Small refactoring

--- lib/minting/money/allocation.rb
+++ lib/minting/money/allocation.rb
@@ -19,41 +19,41 @@ module Mint
 
       subunit = currency.subunit
       amounts = proportions.map { |rate| Rational(amount * rate, whole).round(subunit) }
-      allocate_left_over!(amounts: amounts, left_over: amount - amounts.sum)
+      allocate_left_over(amounts: amounts, left_over: amount - amounts.sum)
     end
 
     # Splits the monetary amount into a given quantity of equal parts.
     # Disperses any fractional subunit rounding differences across the initial slots
     # so that the sum is preserved.
     #
-    # @param quantity [Integer] the number of equal parts to divide the money into (must be > 0)
+    # @param slices [Integer] the number of equal parts to divide the money into (must be > 0)
     # @return [Array<Money>] the list of newly split Money objects
     # @raise [ArgumentError] if quantity is not a positive integer
     #
     # @example Even split
     #   money = Mint.money(10.00, 'USD')
     #   money.split(3) #=> [[USD 3.34], [USD 3.33], [USD 3.33]]
-    def split(quantity)
-      unless  quantity.positive? && quantity.integer?
-        raise ArgumentError,
-              'quantity must be an integer > 0'
-      end
+    def split(slices)
+      raise ArgumentError, 'Slices quantity must be an poitive integer' unless slices.positive? && slices.integer?
 
-      fraction = (amount / quantity).round(currency.subunit)
-      allocate_left_over!(amounts: Array.new(quantity, fraction),
-                          left_over: amount - (fraction * quantity))
+      fraction = (amount / slices).round(currency.subunit)
+      allocate_left_over(amounts: Array.new(slices, fraction),
+                         left_over: amount - (fraction * slices))
     end
 
     private
 
-    def allocate_left_over!(amounts:, left_over:)
+    # Distributes any leftover amount across the allocation slots by adjusting
+    # individual amounts by the currency's minimum unit, and converting to Money.
+    # Caution: amounts array is mutated by this method
+    def allocate_left_over(amounts:, left_over:)
       if left_over.nonzero?
         minimum = currency.minimum_amount
         minimum = -minimum if left_over.negative?
-        last_slot = (left_over / minimum).to_i - 1
-        (0..last_slot).each { |slot| amounts[slot] += minimum }
+        slots_to_adjust = (left_over / minimum).to_i
+        (0...slots_to_adjust).each { |slot| amounts[slot] += minimum }
       end
-      amounts.map { |amount| Money.new(amount, currency) }
+      amounts.map! { |amount| Money.new(amount, currency) }
     end
   end
 end
